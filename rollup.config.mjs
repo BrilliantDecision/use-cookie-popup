d16c7f1e3ef564f09474416f23670db6089e0418
BrilliantDecision <alexmobilepost@gmail.com>
2025-04-29 02:44:12 +0300

    feat:master cleanup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,9 +1,7 @@
-import babel from "@rollup/plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
 import external from "rollup-plugin-peer-deps-external";
 import commonjs from "@rollup/plugin-commonjs";
 import terser from "@rollup/plugin-terser";
-// import typescript from "@rollup/plugin-typescript";
 import typescript from "rollup-plugin-typescript2";
 import dts from "rollup-plugin-dts";
 import packageJson from "./package.json" assert { type: "json" };
