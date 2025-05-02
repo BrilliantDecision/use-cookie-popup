e954cbc83c33d31b94eeb25b5ba084a562e06be7
BrilliantDecision <alexmobilepost@gmail.com>
2025-05-03 00:22:10 +0300

    feat:master added opts, downgrade react version

--- src/hooks/useCookiePopup.ts
+++ src/hooks/useCookiePopup.ts
@@ -1,20 +1,40 @@
 import { useEffect, useState } from "react";
 import { getCookie, setCookie } from "../api/cookieApi";
+import { SerializeOptions } from "cookie";
 
 export interface UseCookieProps {
   name: string;
   showUpDelay?: number;
+  optsCookieDecline?: SerializeOptions;
+  optsCookieAccept?: SerializeOptions;
+  showAlways?: boolean;
+  showIfAccepted?: boolean;
+  showIfDeclined?: boolean;
 }
 
 /**
  * React hook for working with cookie popup
+ * 
+ * Cookie value is 'true' if cookie was accepted, and 'false' if cookie was rejected
  *
  * @param name - name of a cookie,
- * true when cookie was accepted, false when cookie was rejected
- * @param showUpDelay - cookie apperas after this delay,
+ * @param showUpDelay - cookie apperas after this delay
+ * @param optsCookieAccept - opts for setting cookie when accept
+ * @param optsCookieDecline - opts for setting cookie when decline
+ * @param showAlways - isVisible will be true before accepting/declining
+ * @param showIfAccepted - isVisible will be true before accepting
+ * @param showIfDeclined - isVisible will be true before declining
  * @returns isVisible for popup and tools for managing visibity
  */
-export const useCookiePopup = ({ name, showUpDelay }: UseCookieProps) => {
+export const useCookiePopup = ({
+  name,
+  showUpDelay,
+  optsCookieAccept,
+  optsCookieDecline,
+  showAlways,
+  showIfAccepted,
+  showIfDeclined,
+}: UseCookieProps) => {
   const [isVisible, setIsVisible] = useState(false);
 
   const onClose = () => {
@@ -23,18 +43,24 @@ export const useCookiePopup = ({ name, showUpDelay }: UseCookieProps) => {
 
   const onAccept = () => {
     onClose();
-    setCookie(name, true);
+    setCookie(name, true, optsCookieAccept);
   };
 
   const onDecline = () => {
     onClose();
-    setCookie(name, false);
+    setCookie(name, false, optsCookieDecline);
   };
 
   useEffect(() => {
     const cookie = getCookie(name);
+    const isBoolCookie = cookie === "true";
+    const isShowCookie =
+      !cookie ||
+      showAlways ||
+      (showIfAccepted && isBoolCookie) ||
+      (showIfDeclined && !isBoolCookie);
 
-    if (!cookie) {
+    if (isShowCookie) {
       setTimeout(() => setIsVisible(true), showUpDelay);
     }
   }, []);
