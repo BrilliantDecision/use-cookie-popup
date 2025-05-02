cc6b6769b4502ff7f1ba22490df7b45443f5594c
BrilliantDecision <alexmobilepost@gmail.com>
2025-05-02 14:25:16 +0300

    fix:master remove dist, change keywords

--- dist/esm/api/cookieApi.d.ts
+++
@@ -1,5 +0,0 @@
-import { SerializeOptions } from "cookie";
-declare const getCookies: () => Partial<Record<string, string>> | undefined;
-declare const getCookie: (key: string) => string | undefined;
-declare const setCookie: (key: string, data: any, options?: SerializeOptions) => void;
-export { getCookies, getCookie, setCookie };
--- dist/esm/utils/common.d.ts
+++
@@ -1,3 +0,0 @@
-declare const stringify: (value: any) => any;
-declare const decode: (str: string) => string;
-export { stringify, decode };
